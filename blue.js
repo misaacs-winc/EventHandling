cdfb091b4dbb0ef67495db6e9f67daa32040a23e
misaacs-winc <88142732+misaacs-winc@users.noreply.github.com>
2021-10-29 02:40:44 +0200

    Add files by upload

---
+++ blue.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    const x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+};
+
+function changeBackground(color) {
+    document.body.style.background = color;
+}
+
+window.addEventListener("load", function () { changeBackground('blue') });
